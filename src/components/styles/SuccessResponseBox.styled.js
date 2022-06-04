4484af6e120b9a85d066c6edc861e53188740c19
josh-merrell <27826752+josh-merrell@users.noreply.github.com>
2022-06-03 20:38:52 -0600

    adjusted styling colors and margins

--- src/components/styles/SuccessResponseBox.styled.js
+++ src/components/styles/SuccessResponseBox.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const SuccessResponseDropdown = styled.pre`
-  background-color: #ffffff;
+  background-color: #e9eef0;
   border: 7px solid #078aa8;
   border-top: 0px;
   border-radius: 0px 0px 20px 20px;
   padding: 20px 10px;
-  color: #003845;
+  color: black;
   margin-top: 0px;
   font-size: 1.25em;
 `;
