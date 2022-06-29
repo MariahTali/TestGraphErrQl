55afbfe3641e509f85460e470ad3955b5c340e94
Gwen Phillips <gwen.phil@gmail.com>
2022-06-28 20:25:34 -0400

    input box style

--- src/components/LMInputBox.js
+++ src/components/LMInputBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-// function LMInputBox() {
-//   return (
-//     <div>
-//       <h1>Rendering LM Input Box</h1>
-//     </div>
-//   );
-// }
+function LMInputBox() {
+  return (
+    <div>
+      <h1>Rendering LM Input Box</h1>
+    </div>
+  );
+}
 
-// export default LMInputBox;
+export default LMInputBox;
--- src/components/QueryBox.js
+++ src/components/QueryBox.js
@@ -4,26 +4,6 @@ import LMResponseBox from './LMResponseBox';
 import { StyledLMInputBox } from './styles/LMInputBox.styled.js';
 import { StyledLMResponseBox } from './styles/LMOutputBox.styled.js';
 
-// import type { ValidationContext, SDLValidationContext } from 'graphql';
-// import { GraphQLSchema } from 'graphql';
-
-// import CodeMirror from 'codemirror';
-// import 'codemirror/addon/hint/show-hint';
-// import 'codemirror/addon/lint/lint';
-// import 'codemirror-graphql/hint';
-// import 'codemirror-graphql/lint';
-// import 'codemirror-graphql/mode';
-
-CodeMirror.fromTextArea(myTextarea, {
-  mode: 'graphql',
-  lint: {
-    schema: myGraphQLSchema,
-    // validationRules: [ExampleRule],
-  },
-  hintOptions: {
-    schema: myGraphQLSchema,
-  },
-});
 
 function QueryBox() {
   return (
