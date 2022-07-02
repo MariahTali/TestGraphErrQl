4ed603f66a0d5083fb18b40176980ced6292451c
Gwen Phillips <gwen.phil@gmail.com>
2022-07-02 17:54:35 -0400

    Added Codemirror
    Co-authored-by: Mariah 76408932+MariahTali@users.noreply.github.com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import { GraphContextProvider } from './components/Context';
 
 function App() {
   return (
     <div>
-      <Header />
+      <GraphContextProvider>
+        <Header />
+      </GraphContextProvider>
     </div>
   );
 }
